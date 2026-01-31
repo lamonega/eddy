aeb05204b4fe725541e2b5a791a3888f1b9ec044
Laureano F. Lamonega <laureanolamonega@gmail.com>
2026-01-31 18:15:37 +0000

    feat: improve line editing input validation and cleanup

--- eddy.pas
+++ eddy.pas
@@ -3,9 +3,10 @@ uses
     Classes, SysUtils;
 
 var
-    file_name, line_to_add: string;
-    line_to_edit: integer;
+    file_name, line_to_add, input_str: string;
+    line_to_edit, error_code: integer;
     buffer: TStringList;
+    valid_input: boolean;
 begin
     WriteLn('Enter file name: ');
     ReadLn(file_name);
@@ -21,26 +22,37 @@ begin
     
     WriteLn('File content:');
     Write(buffer.Text);
+    WriteLn('Total lines: ', buffer.Count);
     WriteLn;
     
-    WriteLn('Enter line number to edit (1 to ', buffer.Count, '): ');
-    ReadLn(line_to_edit);
-    
-    if (line_to_edit < 1) or (line_to_edit > buffer.Count) then
-    begin
-        WriteLn('Error: Line number out of range.');
-        buffer.Free;
-        Halt(1);
-    end;
+    valid_input := false;
+    repeat
+        Write('Enter line number to edit (1 to ', buffer.Count, '): ');
+        ReadLn(input_str);
+        
+        Val(input_str, line_to_edit, error_code);
+        
+        if error_code <> 0 then
+        begin
+            WriteLn('Error: Please enter a valid number.');
+        end
+        else if (line_to_edit < 1) or (line_to_edit > buffer.Count) then
+        begin
+            WriteLn('Error: Line number must be between 1 and ', buffer.Count, '.');
+        end
+        else
+        begin
+            valid_input := true;
+        end;
+    until valid_input;
     
     WriteLn('Enter new content for line ', line_to_edit, ': ');
     ReadLn(line_to_add);
     
     buffer[line_to_edit - 1] := line_to_add;
-    
     buffer.SaveToFile(file_name);
+    buffer.Free;
     
     WriteLn;
     WriteLn('File saved successfully!');
-    buffer.Free;
 end.
